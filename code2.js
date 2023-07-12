c1caceda77b026bf2666271eba8af4aa1d2e6f38
Ankit Kumar Singh <35650455+ankitrox5813@users.noreply.github.com>
2023-07-13 01:39:42 +0530

    Update code2.js

--- code2.js
+++ code2.js
@@ -2,8 +2,8 @@ function person (firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
     }
-    let person1 = new person ("Shantanu", "Shubham");
-    let person2 = new person ("Anurag", "Mishra"); 
+    let person1 = new person ("Ankit", "Singh");
+    let person2 = new person ("Kunal", "Singh"); 
     
     console.log(person1.firstName); 
     console.log('${person2.firstName} ${person2.lastName}');
